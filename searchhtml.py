c797b21f83759e3e18e107b38bb11f68260ca505
Reno Ren <friendkey@gmail.com>
2011-06-13 08:47:58 +0800

    do not submit image if no main pic found

--- searchhtml.py
+++ searchhtml.py
@@ -18,12 +18,12 @@ class MyParser(sgmllib.SGMLParser):
   def __init__(self,verbose=0):
     sgmllib.SGMLParser.__init__(self,verbose)
     self.url=None
+    self.found=False
 
   def start_img(self,attr):
-    found=False
     for name,value in attr:
       if (self.site=='picplz' and name=="id" and value=="mainImage") or(self.site=='instagram' and name=="class" and value=="photo") or (self.site=='flickr' and name=="alt" and value=="photo") or (self.site=='4sq' and name=="class" and value=="mainPhoto"):
-        found=True
+        self.found=True
       elif name=="src":
         self.url=value
     if found:
@@ -33,4 +33,4 @@ class MyParser(sgmllib.SGMLParser):
 def searchImage(url, content):
   parser=MyParser()
   parser.parse(url,content)
-  return parser.url
+  return parser.found and parser.url
